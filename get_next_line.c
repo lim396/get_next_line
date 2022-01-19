681dabde65ced64fd0c9d8947c6f504aa2923df8
lim <lbooks424@gmail.com>
2022-01-19 18:01:58 +0900

    [clean] strnappend func

--- get_next_line.c
+++ get_next_line.c
@@ -1,41 +1,5 @@
-#include <stdlib.h>
-#include <limits.h>
-#include <unistd.h>
-//#include <errno.h>
-#include <bool.h>
 #include "get_next_line.h"
 
-size_t	ft_strlen(const char *str)
-{
-	size_t	i;
-	
-	if (!str)
-		return (0);
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
-char	*ft_strchr(const char *s, int c)
-{
-	char	*pos_c;
-	char	find_c;
-
-	if (!s)
-		return (NULL);
-	pos_c = NULL;
-	find_c = (char)c;
-	while (*s)
-	{
-		if (*s == find_c)
-			return (char *)s;
-		s++;
-	}
-	if (find_c == '\0')
-		pos_c = (char *)s;
-	return (pos_c);
-}
 
 ssize_t searched_len(const char *s, int c)
 {
@@ -57,48 +21,26 @@ ssize_t searched_len(const char *s, int c)
 
 char *strnappend(char *s1, char *s2, size_t n)
 {
-	size_t	new_arr_size;
-	char	*arr_head;
-	char	*tmp;
+	char	*ret_arr;
+	size_t	i;
+	size_t	j;
 	
 	if (!*s1 && !*s2)
 		return (NULL);
-	new_arr_size = ft_strlen(s1) + n + 1;
-	arr_head = (char *)malloc(new_arr_size + 1);
-	if (!arr_head)
-		return (NULL);
-	tmp = arr_head;
-	while (*s1)
-		*tmp++ = *s1++;
-	while (*s2 && n--)
-		*tmp++ = *s2++;
-	*tmp = '\0';
-	return (arr_head);
-}
-
-char *ft_strcpy(char *dest, const char *src)
-{
-	size_t	i;
-
 	i = 0;
-	while (src[i])
+	j = 0;
+	ret_arr = (char *)malloc(sizeof(char) * (ft_strlen(s1) + n + 1));
+	if (!ret_arr)
+		return (NULL);
+	while (s1[i])
 	{
-		dest[i] = src[i];
+		ret_arr[i] = s1[i];
 		i++;
 	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-char *ft_strdup(const char *s1)
-{
-	char	*cpy_head;
-
-	cpy_head = (char *)malloc(sizeof(char) * (ft_strlen(s1) + 1));
-	if (!cpy_head)
-		return (NULL);
-	cpy_head = ft_strcpy(cpy_head, s1);
-	return (cpy_head);
+	while (s2[j] && n > j)
+		ret_arr[i++] = s2[j++];
+	ret_arr[i] = '\0';
+	return (ret_arr);
 }
 
 char *save_after_newline(char *buf, ssize_t new_line_pos, char *line)
@@ -154,19 +96,6 @@ char	*read_and_throw(int fd, char *buf, char *save_fd)
 	return (save_fd);
 }
 
-bool	null_check(char *checked, char *released)
-{
-	if (!checked)
-	{
-		free(save);
-		save = NULL;
-		free(line);
-		line = NULL;
-		return (true);		
-	}
-	return (false);
-}
-
 char	*get_next_line(int fd)
 {
 	static char	*save[257];
@@ -177,7 +106,7 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || 256 < fd || BUFFER_SIZE <= 0)
 		return (NULL);
-	buf = (char *)malloc(BUFFER_SIZE + 1);m
+	buf = (char *)malloc(BUFFER_SIZE + 1);
 	if (!buf)
 		return (NULL);
 	line = ft_strdup("");
