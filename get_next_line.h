d69a519dc437f281dd06a9e4348ff0f0ac49dff8
lim <lbooks424@gmail.com>
2022-02-01 00:53:18 +0900

    [clean] norm and sep files

---
+++ get_next_line.h
@@ -0,0 +1,16 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <limits.h>
+# include <unistd.h>
+# include <stdbool.h>
+
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strcpy(char *dest, const char *src);
+char	*ft_strdup(const char *s1);
+bool	null_check(char *checked, char *released);
+char	*get_next_line(int fd);
+
+#endif
